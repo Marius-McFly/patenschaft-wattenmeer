a7e146989e195c0437a5492c2b754d9586a8213d
Marius <marius-hofmann@gmx.de>
2021-12-01 17:18:17 +0100

    Add topbar with animation

---
+++ components/IconMenu.vue
@@ -0,0 +1,44 @@
+<template>
+	<div
+		class="w-8 h-8 cursor-pointer"
+		@click="$emit('click')"
+	>
+		<div class="line-top h-[3px] w-full bg-black-default mb-2 rounded" />
+		<div class="line-middle h-[3px] w-8 bg-black-default mb-2 rounded fixed" />
+		<div class="line-middle h-[3px] w-full bg-black-default mb-2 rounded" />
+		<div class="line-bottom h-[3px] w-full bg-black-default rounded" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'IconMenu'
+
+}
+</script>
+
+<style scoped>
+
+div {
+  transition: all 500ms;
+}
+
+.active .line-middle {
+  transform: rotate(45deg);
+}
+
+.active .line-middle:nth-child(2) {
+  transform: rotate(-45deg);
+}
+
+.active .line-top {
+  transform: translateY(11px);
+  opacity: 0;
+}
+
+.active .line-bottom {
+  transform: translateY(-11px);
+  opacity: 0;
+}
+
+</style>
--- components/TheTopbar.vue
+++ components/TheTopbar.vue
@@ -1,59 +1,99 @@
 <template>
-	<div class="py-4">
+	<nav
+		class="py-4 px-6 transition-all duration-500 ease-in-out bg-gray-300"
+		:class="isOpen ? 'h-screen bg-opacity-90' : 'h-20 bg-opacity-50'"
+	>
+		<NuxtLink to="/">
+			<div
+				class="max-w-[104px] relative z-10"
+				v-html="require(`~/static/svg/wattenmeer-logo.svg?raw`)"
+			/>
+		</NuxtLink>
 		<div
-			class="w-28"
-			v-html="require(`~/static/svg/wattenmeer-logo.svg?raw`)"
+			class="flex items-center w-full justify-end fixed right-8 top-8 desktop:hidden z-0"
+		>
+			<IconMenu
+				class=""
+				:class="isOpen ? 'active' : ''"
+				@click="toggleNav"
+			/>
+		</div>
+		<PageNavigation
+			class="transition-all mt-12 duration-[375ms] ease-in-out desktop:hidden"
+			:class="isOpen ? 'opacity-100 transform' : 'opacity-0 transform translate-y-[-480px]'"
+			:navigation-items="navigationItems"
 		/>
-		<ul class="flex flex-col items-center">
-			<li
-				v-for="navigationItem in navigationItems"
-				:key="navigationItem.name"
-				class="mb-4 last:mb-0"
-			>
-				{{ navigationItem.name }}
-			</li>
-		</ul>
-	</div>
+	</nav>
 </template>
 
 <script>
 export default {
 	data () {
 		return {
+			isOpen: false,
 			navigationItems: [
 				{
-					name: 'Nationalpark'
+					name: 'Nationalpark',
+					routePath: '/nationalpark'
 				},
 
 				{
-					name: 'Projekte'
+					name: 'Projekte',
+					routePath: '/projekte'
 				},
 
 				{
-					name: 'Patenschaft'
+					name: 'Patenschaft',
+					routePath: '/patenschaft'
+
 				},
 
 				{
-					name: 'Mitmachen'
+					name: 'Mitmachen',
+					routePath: '/mitmachen'
 				},
 
 				{
-					name: 'Besuchen'
+					name: 'Besuchen',
+					routePath: '/besuchen'
+
 				},
 
 				{
-					name: 'Presse'
+					name: 'Presse',
+					routePath: '/presse'
 				},
 
 				{
-					name: 'Impressum'
+					name: 'Impressum',
+					routePath: '/impressum'
+
 				},
 
 				{
-					name: 'Kontakt'
+					name: 'Kontakt',
+					routePath: '/kontakt'
 				}
 			]
 		}
+	},
+
+	mounted () {
+		this.$nextTick(() => {
+			document.body.classList.remove('overflow-hidden')
+		})
+	},
+
+	methods: {
+		toggleNav () {
+			this.isOpen = !this.isOpen
+			if (this.isOpen) {
+				document.body.classList.add('overflow-hidden')
+			} else {
+				document.body.className = document.body.className.replace('overflow-hidden', '')
+			}
+		}
+
 	}
 
 }
