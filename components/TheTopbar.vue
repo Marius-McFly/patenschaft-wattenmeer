847727d5bc6ae40342c210f7c05d6c71ea5f1307
Marius <marius-hofmann@gmx.de>
2021-12-01 10:50:26 +0100

    Add images & new folders

---
+++ components/TheTopbar.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<div />
+		<ul>
+			<li>Huhu</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<Tutorial />
+	<TheTopbar />
 </template>
 
 <script>
