64f8990aec148fec2ec49d4fc57cf00610d03184
Marius <marius-hofmann@gmx.de>
2021-12-03 14:39:19 +0100

    Add AppButton

---
+++ components/AppButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<button
+		:class="['font-semibold text-white w-full tracking-wide uppercase px-8 py-3 text-center rounded-full cursor-pointer', {'border-2 border-solid border-white': borderButton}, {'bg-blue-default': primaryButton} ]"
+	>
+		<slot />
+	</button>
+</template>
+
+<script>
+export default {
+
+	props: {
+		borderButton: {
+			type: Boolean,
+			default: false
+		},
+
+		primaryButton: {
+			type: Boolean,
+			default: false
+		}
+	}
+
+}
+</script>
+
+<style>
+
+</style>
