f2ef740ed8669431a602ddf42dc954cb212190bc
Marius <marius-hofmann@gmx.de>
2021-12-03 14:42:50 +0100

    Implement componentes into index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
 	<main>
 		<TheTopbar />
-		<p>dwdadwadwdawdaw</p>
+		<div class="h-screen">
+			<PageHero
+				:title="content.pageHero.title"
+				:subtitle="content.pageHero.subtitle"
+				:image="content.pageHero.imagePath"
+				:button-text="content.pageHero.buttonText"
+			/>
+		</div>
+		<div class="custom-container">
+			<ImageText
+				:title="content.imageText[0].title"
+				:text="content.imageText[0].text"
+				:image="content.imageText[0].imagePath"
+				class="mb-12"
+			/>
+			<ImageText
+				:text="content.imageText[1].text"
+				:image="content.imageText[1].imagePath"
+				:button-text="content.imageText[1].buttonText"
+			/>
+		</div>
 	</main>
 </template>
 <router>
