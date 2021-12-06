90622bf87580bc935379132b062ad4176a644431
Marius <marius-hofmann@gmx.de>
2021-12-06 15:29:33 +0100

    New hero version

--- components/PageHero.vue
+++ components/PageHero.vue
@@ -1,25 +1,67 @@
 <template>
-	<div class="absolute inset-0 h-screen flex justify-center items-center">
+	<div class="h-screen">
 		<img
-			:src="image"
+			v-if="fullHeroImage"
+			:src="fullHeroImage"
 			:class="['absolute object-cover h-full w-full desktop:max-w-[1920px] widescreen:max-w-[3840px] rounded-br-[100px] rounded-bl-[100px] shadow-2xl', {'transform scale-x-[-1]': isLanding}]"
 			alt=""
 		>
-		<div class="absolute opacity-50 bg-black-default h-full w-full desktop:max-w-[1920px] widescreen:max-w-[3840px] rounded-br-[100px] rounded-bl-[100px]" />
-		<div class="relative z-20 flex flex-col justify-center text-white h-full w-full">
-			<div class="custom-container pt-40 w-full">
-				<h1 class="mb-4 desktop:max-w-[800px]">
-					{{ title }}
-				</h1>
-				<div class="w-[104px] bg-white h-1 rounded mb-4" />
-				<h2 class="mb-10 desktop:mb-12 desktop:max-w-[600px] widescreen:mb-16">
-					{{ subtitle }}
-				</h2>
-				<AppButton
-					border-button
-				>
-					{{ buttonText }}
-				</AppButton>
+		<img
+			v-if="!fullHeroImage"
+			:src="images[0]"
+			class="absolute mobile:top-1/2 mobile:object-[100px] phablet:object-[200px] phablet:top-1/2 tablet:top-1/4 desktop:top-64 desktop:left-auto z-30 opacity-40 desktop:object-[500px]"
+			alt=""
+		>
+		<div
+			v-if="!fullHeroImage"
+			class="absolute w-52 h-[600px] right-32 desktop:w-96 bg-blue-100 desktop:h-[800px] rounded-br-[40px] rounded-bl-[40px] top-0 desktop:right-96"
+		/>
+		<div
+			v-if="fullHeroImage"
+			class="absolute opacity-50 bg-black-default h-full w-full desktop:max-w-[1920px] widescreen:max-w-[3840px] rounded-br-[100px] rounded-bl-[100px]"
+		/>
+		<div :class="['relative z-10 flex flex-col justify-center text-white h-full w-full', {'text-black-default': !fullHeroImage }]">
+			<div :class="['custom-container pt-20 w-full', {'flex pb-0': !fullHeroImage}]">
+				<div :class="{'flex-1 desktop:mr-20': !fullHeroImage}">
+					<h1 class="mb-2 desktop:max-w-[800px]">
+						{{ title }}
+					</h1>
+					<div
+						v-if="fullHeroImage"
+						class="w-[104px] bg-white h-1 rounded mb-4"
+					/>
+					<h2 class="mb-8 desktop:mb-12 desktop:max-w-[600px] widescreen:mb-16">
+						{{ subtitle }}
+					</h2>
+					<div :class="{'mobile:block flex-1 desktop:hidden': !fullHeroImage}">
+						<img
+							v-if="!fullHeroImage"
+							class="mobile:mb-12 phablet:mb-20 mobile:rounded-3xl desktop:rounded-[40px] object-cover shadow-2xl"
+							:src="images[1]"
+							alt=""
+						>
+					</div>
+					<AppButton
+						v-if="fullHeroImage"
+						border-button
+					>
+						{{ buttonText }}
+					</AppButton>
+					<AppButton
+						v-if="!fullHeroImage"
+						primary-button
+					>
+						{{ buttonText }}
+					</AppButton>
+				</div>
+				<div :class="{'mobile:hidden flex-1 desktop:block': !fullHeroImage}">
+					<img
+						v-if="!fullHeroImage"
+						class="mobile:rounded-3xl desktop:rounded-[40px] object-cover shadow-2xl"
+						:src="images[1]"
+						alt=""
+					>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -41,9 +83,14 @@ export default {
 			required: true
 		},
 
-		image: {
+		fullHeroImage: {
 			type: String,
 			default: ''
+		},
+
+		images: {
+			type: Array,
+			default: () => ([])
 		},
 
 		buttonText: {
--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,6 +1,13 @@
 <template>
 	<main>
 		<TheTopbar />
+		<PageHero
+			:title="content.pageHero.title"
+			:subtitle="content.pageHero.subtitle"
+			:images="content.pageHero.images"
+			:button-text="content.pageHero.buttonText"
+			is-landing
+		/>
 	</main>
 </template>
 <router>
@@ -10,6 +17,34 @@
 </router>
 <script>
 export default {
-	name: 'Projects'
+	name: 'Projects',
+
+	data () {
+		return {
+			content: {
+				pageHero: {
+					title: 'Projekte',
+					subtitle: 'Helfe mit das Wattenmeer zu erhalten und es vor einer Verschmutzung oder äußeren Einflüssen zu bewahren. Deine Patenschaft kann etwas bewegen',
+					images: [
+						'/svg/wattenmeer-bildmarke.svg',
+						'img/kelli-mcclintock-wBgAVAGjzFg-unsplash.jpg'
+					],
+					buttonText: 'Hier mitmachen →'
+				},
+				imageText: [
+					{
+						title: 'Wattenmeer schützen',
+						text: 'Der Schutz der Natur ist die Kernaufgabe der Wattenmeer-Nationalparks. Diese Aufgabe wird mit verschiedenen Instrumenten umgesetzt, von lokalen Maßnahmen über großräumige Projekte bis hin zur internationalen Zusammenarbeit. In Deutschland ist der Schutz des Wattenmeeres vor allem durch die Nationalparks an den Küsten Niedersachsens, Hamburgs und Schleswig-Holsteins sichergestellt.',
+						imagePath: '/img/sevak-oJbF_4qfrjE-unsplash.jpg'
+					},
+					{
+						text: 'Mit der Anerkennung als UNESCO Weltnaturerbe sind Deutschland, Dänemark und die Niederlande auch gegenüber der Weltgemeinschaft verpflichtet, den außergewöhnlichen universellen Wert der Natur im Wattenmeer zu erhalten. Darüber hinaus gehört Naturschutz zu den Funktionen eines UNESCO-Biosphärenreservats. ',
+						imagePath: '/img/christian-paul-stobbe-bJEasxPTOUw-unsplash.jpg',
+						buttonText: 'Hier mitmachen →'
+					}
+				]
+			}
+		}
+	}
 }
 </script>
