e52278ec488480db029b68f1ee5516673e660d97
Marius <marius-hofmann@gmx.de>
2021-12-07 16:11:56 +0100

    Add Projects page, implement components & data

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -8,6 +8,33 @@
 			:button-text="content.pageHero.buttonText"
 			is-landing
 		/>
+		<div class="custom-container pb-20 phablet:py-20 desktop:py-[104px]">
+			<ImageText
+				:title="content.imageText[0].title"
+				:text="content.imageText[0].text"
+				:image="content.imageText[0].imagePath"
+				is-first-item
+			/>
+		</div>
+		<div class="pb-20 desktop:pb-[104px]">
+			<div class="custom-container py-20 desktop:px-20 desktop:py-[104px] bg-blue-default desktop:rounded-[100px] mobile:rounded-[40px] phablet:rounded-[80px] flex flex-col items-center">
+				<h3 class="text-center text-white">
+					{{ content.cardSection.title }}
+				</h3>
+				<div class="w-[104px] bg-white h-1 rounded mt-8 mb-20" />
+				<AppCardCollection
+					:items="content.cardSection.cardItems"
+					is-icon-card
+				/>
+				<AppButton
+					class="mt-20"
+					border-button
+				>
+					{{ content.cardSection.buttonText }}
+				</AppButton>
+			</div>
+		</div>
+		<TheFooter />
 	</main>
 </template>
 <router>
@@ -33,16 +60,41 @@ export default {
 				},
 				imageText: [
 					{
-						title: 'Wattenmeer schützen',
-						text: 'Der Schutz der Natur ist die Kernaufgabe der Wattenmeer-Nationalparks. Diese Aufgabe wird mit verschiedenen Instrumenten umgesetzt, von lokalen Maßnahmen über großräumige Projekte bis hin zur internationalen Zusammenarbeit. In Deutschland ist der Schutz des Wattenmeeres vor allem durch die Nationalparks an den Küsten Niedersachsens, Hamburgs und Schleswig-Holsteins sichergestellt.',
+						title: 'Hilfe für das Wattenmeer',
+						text: 'Klimawandel ist bei uns angekommen – Meeresspiegelanstieg, höhere Wasser- und Lufttemperaturen, Veränderungen der Niederschlagsverhältnisse, Verschiebungen im Artengefüge der Ökosysteme – all das sind Klimawandelfolgen, die vor unserer Haustür sichtbar sind, besonders in den Küstenbereichen. An Schleswig-Holsteins Westküste liegt im Küstenstreifen zwischen dänischer Grenze im Norden und Elbmündung im Süden das Schleswig-Holsteinische Wattenmeer, Teil von einem der größten Nationalparke Europas und seit 2009 als UNESCO-Weltnaturerbe anerkannt. Es ist ein Lebensraum von globaler Bedeutung, der wie kaum ein anderer an wechselnde Wasserstände angepasst ist. Der Naturwert dieser amphibischen Landschaft strahlt zusammen mit der kulturellen Einzigartigkeit weit über die Region hinaus.',
 						imagePath: '/img/sevak-oJbF_4qfrjE-unsplash.jpg'
-					},
-					{
-						text: 'Mit der Anerkennung als UNESCO Weltnaturerbe sind Deutschland, Dänemark und die Niederlande auch gegenüber der Weltgemeinschaft verpflichtet, den außergewöhnlichen universellen Wert der Natur im Wattenmeer zu erhalten. Darüber hinaus gehört Naturschutz zu den Funktionen eines UNESCO-Biosphärenreservats. ',
-						imagePath: '/img/christian-paul-stobbe-bJEasxPTOUw-unsplash.jpg',
-						buttonText: 'Hier mitmachen →'
 					}
-				]
+				],
+				cardSection: {
+					title: 'Die Patenschaft fördert die Zielsetzungen von Nationalpark und Biosphärenreservat ideell und materiell durch Aktivitäten insbesondere zur…',
+					buttonText: 'Hier mitmachen →',
+					cardItems: [
+						{
+							icon: 'icon-awesome-envira.svg',
+							text: 'Förderung des Umweltbewusstseins und der Umwelterziehung'
+						},
+						{
+							icon: 'icon-awesome-hands-helping.svg',
+							text: 'Förderung von Projekten zur nachhaltigen Entwicklung der Wattenmeerregion'
+
+						},
+						{
+							icon: 'icon-awesome-hand-holding.svg',
+							text: 'Hebung von Bedeutung und Ansehen des Nationalparks, des Biosphärenreservates und des Weltnaturerbes'
+
+						},
+						{
+							icon: 'icon-awesome-globe-europe.svg',
+							text: 'Nationalen und internationalen Zusammenarbeit'
+
+						},
+						{
+							icon: 'icon-awesome-hands.svg',
+							text: 'Erhaltung der natürlichen Lebensräume und Lebensgemeinschaften samt ihrer Pflanzen – und Tierwelt'
+
+						}
+					]
+				}
 			}
 		}
 	}
