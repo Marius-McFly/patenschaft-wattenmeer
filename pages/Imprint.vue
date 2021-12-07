1ffa9247e4c70fd6ca1e989d497c73602a0cf730
Marius <marius-hofmann@gmx.de>
2021-12-07 16:12:12 +0100

    Add imprint

--- pages/Imprint.vue
+++ pages/Imprint.vue
@@ -1,6 +1,15 @@
 <template>
 	<main>
 		<TheTopbar />
+		<div class="custom-container h-screen w-full flex flex-col justify-center text-center">
+			<h1 class="text-2xl mb-6">
+				This is a school project :)
+			</h1>
+			<p class="text">
+				I do not own any images, fonts or media files on this page.<br>
+				Be gentle, thanks.
+			</p>
+		</div>
 	</main>
 </template>
 
