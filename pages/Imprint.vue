f36256aa30789f701ddbd017417a94d64878702f
Marius <marius-hofmann@gmx.de>
2021-12-07 18:10:01 +0100

    Add footer to imprint and add responsive styling

--- pages/Imprint.vue
+++ pages/Imprint.vue
@@ -1,14 +1,16 @@
 <template>
-	<main>
+	<main class="bg-blue-100">
 		<TheTopbar />
-		<div class="custom-container h-screen w-full flex flex-col justify-center text-center">
-			<h1 class="text-2xl mb-6">
-				This is a school project :)
-			</h1>
-			<p class="text">
-				I do not own any images, fonts or media files on this page.<br>
-				Be gentle, thanks.
-			</p>
+		<div class="h-screen w-full flex flex-col justify-end text-center">
+			<div class="h-full flex flex-col justify-center pt-80 desktop:pt-60 px-6">
+				<h1 class="mobile:text-base desktop:text-2xl mb-6">
+					This is a school project :)
+				</h1>
+				<p class="text">
+					I do not own any images, fonts or media files on this page. Be gentle, thanks.
+				</p>
+			</div>
+			<TheFooter />
 		</div>
 	</main>
 </template>
