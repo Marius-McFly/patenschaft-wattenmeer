648422354b56efc9385ce4c7de0e7cc7fd2cd52f
Marius <marius-hofmann@gmx.de>
2021-12-08 18:32:33 +0100

    Add white version of button

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,10 +1,9 @@
 <template>
 	<button
-		:class="['font-semibold text-white w-full tracking-wide uppercase px-8 py-3 text-center rounded-full cursor-pointer desktop:max-w-[355px] desktop:py-4 desktop:text-tiny', {'border-2 border-solid border-white': borderButton}, {'bg-blue-default': primaryButton} ]"
+		:class="['font-semibold w-full tracking-wide uppercase px-8 py-3 text-center rounded-full cursor-pointer desktop:max-w-[355px] desktop:py-4 desktop:text-tiny', {'border-2 border-solid border-white text-white': borderButton}, {'bg-blue-default text-white': primaryButton}, {'bg-wh bg-white text-blue-default': isWhite}]"
+		@click="onClick()"
 	>
-		<NuxtLink to="/mitmachen">
-			<slot />
-		</NuxtLink>
+		<slot />
 	</button>
 </template>
 
@@ -18,9 +17,21 @@ export default {
 			default: false
 		},
 
+		isWhite: {
+			type: Boolean,
+			default: false
+		},
+
 		primaryButton: {
 			type: Boolean,
 			default: false
+		}
+
+	},
+
+	methods: {
+		onClick ($event) {
+			this.$emit('click', $event)
 		}
 	}
 
