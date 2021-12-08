0fbb262122a375e4fb2e7d937d1968bddfbd4e8d
Marius <marius-hofmann@gmx.de>
2021-12-08 09:59:17 +0100

    Add section hero

--- pages/JoinIn.vue
+++ pages/JoinIn.vue
@@ -2,14 +2,14 @@
 	<main>
 		<TheTopbar />
 		<PageHero
-			:title="content.pageHero.title"
-			:subtitle="content.pageHero.subtitle"
-			:quote-author="content.pageHero.quoteAuthor"
-			:full-hero-image="content.pageHero.imagePath"
-			:button-text="content.pageHero.buttonText"
+			:title="content.pageHero[0].title"
+			:subtitle="content.pageHero[0].subtitle"
+			:quote-author="content.pageHero[0].quoteAuthor"
+			:full-hero-image="content.pageHero[0].imagePath"
+			:button-text="content.pageHero[0].buttonText"
 			is-landing
 		/>
-		<div class="custom-container py-20 desktop:py-[104px] text-left">
+		<div class="custom-container flex items-center flex-col phablet:block py-20 desktop:py-[104px] text-left">
 			<div class="mb-12 max-w-2xl phablet:mb-20 desktop:mb-[104px]">
 				<h3 class="mb-4 phablet:mb-6">
 					{{ content.cardSection.title }}
@@ -22,7 +22,15 @@
 				:items="content.cardSection.cardItems"
 			/>
 		</div>
-		<TheFooter />
+		<PageHero
+			:title="content.pageHero[1].title"
+			:subtitle="content.pageHero[1].subtitle"
+			:quote-author="content.pageHero[1].quoteAuthor"
+			:full-hero-image="content.pageHero[1].imagePath"
+			:button-text="content.pageHero[1].buttonText"
+			is-section
+		/>
+		<TheFooter class="pt-40 desktop:pt-60" />
 	</main>
 </template>
 <router>
@@ -37,54 +45,56 @@ export default {
 	data () {
 		return {
 			content: {
-				pageHero: {
-					title: 'Mitmachen',
-					subtitle: '„Wir sind die erste Generation, die die Armut beenden kann und die letzte Generation, die Maßnahmen ergreifen kann, um die schlimmsten Auswirkungen des Klimawandels zu vermeiden.“',
-					quoteAuthor: 'Ban Ki-moon',
-					imagePath: '/img/510489_neu.jpg',
-					buttonText: 'Hier mitmachen →'
-				},
+				pageHero: [
+					{
+						title: 'Mitmachen',
+						subtitle: '„Wir sind die erste Generation, die die Armut beenden kann und die letzte Generation, die Maßnahmen ergreifen kann, um die schlimmsten Auswirkungen des Klimawandels zu vermeiden.“',
+						quoteAuthor: 'Ban Ki-moon',
+						imagePath: '/img/510489_neu.jpg',
+						buttonText: 'Hier mitmachen →'
+					},
+					{
+						title: 'Werde aktiv und mache mit!',
+						subtitle: 'Es gibt zahlreiche Möglichkeiten, in den Nationalparks mitzuarbeiten. Freiwillige Dienste, verschiedene Formen von Praktika - und manchmal gibt es auch eine freie Stelle in den Nationalparkverwaltungen.',
+						imagePath: '/img/Wattenmeer_Nordsee.jpg',
+						buttonText: 'Hier mitmachen →'
+					}
+				],
 				cardSection: {
 					title: 'Nachhaltigkeitsziele',
 					subtitle: 'Gestaltung eines guten Lebens für alle innerhalb der planetaren Grenzen gegeben. An diesen Zielen, die u.a. die Dimensionen Soziales, Umwelt, Kultur und Wirtschaft berücksichtigen, orientiert sich auch das Konzept der Lernwerkstatt. Schwerpunktmäßig richtet sich dieses auf sechs der 17 Nachhaltigkeitsziele aus:',
 					buttonText: 'Hier mitmachen →',
 					cardItems: [
 						{
-							icon: 'icon-awesome-envira.svg',
 							number: '01',
 							title: 'Qualität der Bildung',
 							text: 'Inklusive, gleichberechtigte und hochwertige Bildung gewährleisten und Möglichkeiten lebenslangen Lernens für alle fördern.'
 						},
 						{
-							icon: 'icon-awesome-hands-helping.svg',
 							number: '02',
 							title: 'Nachhaltige Städte und Gemeinden',
 							text: 'Städte und Siedlungen inklusiv, sicher, widerstandsfähig und nachhaltig gestalten. Verantwortungsvoll'
 
 						},
 						{
-							icon: 'icon-awesome-hand-holding.svg',
 							number: '03',
 							title: 'Konsumieren und Produzieren',
 							text: 'Nachhaltige Konsum- und Produktionsmuster sicherstellen.'
 
 						},
 						{
-							icon: 'icon-awesome-globe-europe.svg',
 							number: '04',
 							title: 'Handeln für den Klimaschutz',
 							text: 'Umgehend Maßnahmen zur Bekämpfung des Klimawandels und seiner Auswirkungen ergreifen.'
 
 						},
 						{
-							icon: 'icon-awesome-hands.svg',
 							number: '05',
 							title: 'Leben unter Wasser',
 							text: 'Ozeane, Meere und Meeresressourcen im Sinne nachhaltiger Entwicklung erhalten und nachhaltig nutzen.'
 
 						},
 						{
-							icon: 'icon-awesome-hands.svg',
 							number: '06',
 							title: 'Leben an Land',
 							text: 'Landökosysteme schützen, wiederherstellen und ihre nachhaltige Nutzung fördern, Wälder nachhaltig bewirtschaften, Wüstenbildung bekämpfen, Bodendegradation beenden und umkehren und dem Verlust der biologischen Vielfalt ein Ende setzen.'
