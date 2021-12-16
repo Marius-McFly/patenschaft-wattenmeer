1e95289cbd5b51ce0c164b9b7a6ca053c6313c03
Marius <marius-hofmann@gmx.de>
2021-12-16 17:24:51 +0100

    Fix button font size

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<button
-		:class="['font-semibold w-full transition-all duration-500 tracking-[0.075em] uppercase px-8 py-3 text-center rounded-full cursor-pointer desktop:max-w-[355px] desktop:py-4 desktop:text-tiny border-2 border-solid border-transparent', {'border-solid border-white text-white hover:text-blue-default hover:bg-white': borderButton}, {'bg-blue-default text-white hover:bg-white hover:border-blue-default hover:text-blue-default': primaryButton}, {'bg-wh bg-white text-blue-default hover:text-white hover:bg-transparent': isWhite}]"
+		:class="['font-semibold w-full transition-all duration-500 tracking-[0.075em] uppercase px-8 py-3 text-center rounded-full cursor-pointer desktop:max-w-[355px] desktop:py-4 desktop:text-sm border-2 border-solid border-transparent', {'border-solid border-white text-white hover:text-blue-default hover:bg-white': borderButton}, {'bg-blue-default text-white hover:bg-white hover:border-blue-default hover:text-blue-default': primaryButton}, {'bg-wh bg-white text-blue-default hover:text-white hover:bg-transparent': isWhite}]"
 		@click="onClick()"
 	>
 		<slot />
