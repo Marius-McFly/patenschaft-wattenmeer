18b2c0cc294618c595a9a243ff1370daa2375aa3
Marius <marius-hofmann@gmx.de>
2021-12-21 10:13:51 +0100

    Decrease topbar height

--- components/TheTopbar.vue
+++ components/TheTopbar.vue
@@ -1,11 +1,11 @@
 <template>
 	<nav
-		class="fixed z-[1000] w-full flex items-center justify-center py-4 px-6 transition-all duration-500 ease-in-out bg-white shadow-2xl desktop:h-40 rounded-br-3xl rounded-bl-3xl desktop:rounded-br-[60px] desktop:rounded-bl-[60px] desktop:px-10"
+		class="fixed z-[1000] w-full flex items-center justify-center py-4 px-6 transition-all duration-500 ease-in-out bg-white shadow-2xl desktop:h-32 rounded-br-3xl rounded-bl-3xl desktop:rounded-br-[60px] desktop:rounded-bl-[60px] desktop:px-10"
 		:class="isOpen ? 'h-screen py-20' : 'h-20'"
 	>
 		<div :class="['desktop:grid desktop:grid-cols-[max-content,1fr,max-content] desktop:items-center desktop:justify-center w-full desktop:max-w-7xl', {'h-auto': isOpen}, {'h-20': !isOpen}]">
 			<NuxtLink to="/">
-				<img class="top-4 z-50 left-8 fixed w-full max-w-[104px] desktop:ml-8 desktop:static desktop:max-w-[180px]" src="/svg/wattenmeer-logo.svg" alt="">
+				<img class="top-4 z-50 left-8 fixed w-full max-w-[104px] desktop:ml-8 desktop:static desktop:max-w-[160px]" src="/svg/wattenmeer-logo.svg" alt="">
 			</NuxtLink>
 			<PageNavigation class="hidden desktop:grid justify-self-end items-center mt-[3px]" :navigation-items="navigationItems" />
 			<NuxtLink to="/kontakt">
